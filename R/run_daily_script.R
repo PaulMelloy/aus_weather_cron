9c7d1d90f8e13ecf4660b52cf3842294b54731d3
PaulMelloy <paul@melloy.com.au>
2024-12-26 09:12:21 +1000

    update to new epiphytoolR version

--- R/run_daily_script.R
+++ R/run_daily_script.R
@@ -9,11 +9,11 @@ if("epiphytoolR" %in% installed.packages()[,"Package"] == FALSE){
                            lib = "/home/shared/lib/R")
 }
 
-working_dir <- path.expand("~/") # default to users home directory
+working_dir <- path.expand("~") # default to users home directory
 
 library(data.table)
 library(epiphytoolR)
-source(paste0(working_dir,"aus_weather_cron/R/get_bom_observations.R"))
+#source(paste0(working_dir,"aus_weather_cron/R/get_bom_observations.R"))
 
 ## -----------------        Settings        -----------------
 SLEEP <- TRUE
@@ -32,7 +32,7 @@ if(dir.exists(weather_path) == FALSE) dir.create(weather_path,recursive = TRUE)
 aus_states <- c("QLD", "NSW", "NT", "TAS","WA", "VIC", "SA")
 lapply(aus_states,function(STATE){
   if(SLEEP) {
-    Sys.sleep(round(runif(1, 0, 240))) # wait randomly between 0 amd 4 minutes
+    Sys.sleep(round(runif(1, 0, 240))) # wait randomly between 0 and 4 minutes
   }
   get_bom_observations(ftp_url = "ftp://ftp.bom.gov.au/anon/gen/fwo/",
                        download_location = dl_path,
@@ -46,53 +46,53 @@ lapply(aus_states,function(STATE){
 # Uncompress select stations and merge the data into one file for downstream
 #  Processing
 
-# North Tamborine: IDQ60910.99123.axf
-merge_axf_weather(File_compressed = paste0(dl_path,dl_time,"_IDQ60910.tgz"),
-                  File_axf = "IDQ60910.99123.axf",
-                  File_formatted = "23-24_NTamborine.csv",
-                  base_dir = weather_path
+# North Tamborine: IDQ60910.99123.json
+merge_weather(File_compressed = paste0(dl_path,dl_time,"_IDQ60910.tgz"),
+              station_file = "IDQ60910.99123.json",
+              File_formatted = "23-24_NTamborine.csv",
+              base_dir = weather_path
 )
 
-# Gatton IDQ60910.94562.axf
-merge_axf_weather(File_compressed = paste0(dl_path,dl_time,"_IDQ60910.tgz"),
-                  File_axf = "IDQ60910.94562.axf",
-                  File_formatted = "23-24_Gatton_weather_obs.csv",
-                  base_dir = weather_path
+# Gatton IDQ60910.94562.json
+merge_weather(File_compressed = paste0(dl_path,dl_time,"_IDQ60910.tgz"),
+              station_file = "IDQ60910.94562.json",
+              File_formatted = "23-24_Gatton_weather_obs.csv",
+              base_dir = weather_path
 )
 
 # Create weather for Applethorpe
-merge_axf_weather(File_compressed = paste0(dl_path,dl_time,"_IDQ60910.tgz"),
-                  File_axf = "IDQ60910.94553.axf",
-                  File_formatted = "23-24_Applethorpe.csv",
-                  base_dir = weather_path
+merge_weather(File_compressed = paste0(dl_path,dl_time,"_IDQ60910.tgz"),
+              station_file = "IDQ60910.94553.json",
+              File_formatted = "23-24_Applethorpe.csv",
+              base_dir = weather_path
 )
 
 # Update weather for Mildura Airport
-merge_axf_weather(File_compressed = paste0(dl_path,dl_time,"_IDV60910.tgz"),
-                  File_axf = "IDV60910.94693.axf",
-                  File_formatted = "23-24_MilduraAP.csv",
-                  base_dir = weather_path
+merge_weather(File_compressed = paste0(dl_path,dl_time,"_IDV60910.tgz"),
+              station_file = "IDV60910.94693.json",
+              File_formatted = "23-24_MilduraAP.csv",
+              base_dir = weather_path
 )
 
 # Update weather for Walpeup Research station.csv
-merge_axf_weather(File_compressed = paste0(dl_path,dl_time,"_IDV60910.tgz"),
-                  File_axf = "IDV60910.95831.axf",
-                  File_formatted = "23-24_WalpeupResearch.csv",
-                  base_dir = weather_path
+merge_weather(File_compressed = paste0(dl_path,dl_time,"_IDV60910.tgz"),
+              station_file = "IDV60910.95831.json",
+              File_formatted = "23-24_WalpeupResearch.csv",
+              base_dir = weather_path
 )
 
 # Update weather for Renmark  station.csv
-merge_axf_weather(File_compressed = paste0(dl_path,dl_time,"_IDS60910.tgz"),
-                  File_axf = "IDS60910.95687.axf",
-                  File_formatted = "23-24_RenmarkAP.csv",
-                  base_dir = weather_path
+merge_weather(File_compressed = paste0(dl_path,dl_time,"_IDS60910.tgz"),
+              station_file = "IDS60910.95687.json",
+              File_formatted = "23-24_RenmarkAP.csv",
+              base_dir = weather_path
 )
 
 # Update weather for loxton station.csv
-merge_axf_weather(File_compressed = paste0(dl_path,dl_time,"_IDS60910.tgz"),
-                  File_axf = "IDS60910.94682.axf",
-                  File_formatted = "23-24_LoxtonResearch.csv",
-                  base_dir = weather_path
+merge_weather(File_compressed = paste0(dl_path,dl_time,"_IDS60910.tgz"),
+              station_file = "IDS60910.94682.json",
+              File_formatted = "23-24_LoxtonResearch.csv",
+              base_dir = weather_path
 )
 
 
@@ -105,7 +105,7 @@ merge_axf_weather(File_compressed = paste0(dl_path,dl_time,"_IDS60910.tgz"),
 # Update weather for Witchcliffe station.csv
 # errors because some weather data files are empty
 # merge_axf_weather(File_compressed = paste0("/homevol/pmelloy/Weather observations/tgz/240101_1500_IDW60910.tgz"),
-#                   File_axf = "IDW60910.95641.axf",
+#                   station_file = "IDW60910.95641.json",
 #                   File_formatted = "23-24_WitchcliffeWest.csv",
 #                   base_dir = weather_path
 # )
@@ -121,17 +121,17 @@ merge_axf_weather(File_compressed = paste0(dl_path,dl_time,"_IDS60910.tgz"),
 # for(i in zipfiles){
 #    # Create weather for Applethorpe
 #    #  Stanthorpe only records 3 hourly
-#    merge_axf_weather(File_compressed = paste0(dl_path,i),
-#                      File_axf = "IDS60910.94682.axf",
-#                      File_formatted = "23-24_LoxtonResearch.csv",
-#                      base_dir = weather_path,
-#                      verbose = TRUE
+#    merge_weather(File_compressed = paste0(dl_path,i),
+#                 station_file = "IDS60910.94682.json", 
+#                   File_formatted = "23-24_LoxtonResearch.csv",
+#                   base_dir = weather_path,
+#                   verbose = TRUE
 #    )
 #
 #  }
-#    merge_axf_weather(File_compressed = "/homevol/pmelloy/Weather observations/tgz/230825_1513_IDW60910.tgz",
-#                      File_axf = "IDW60910.95641.axf",
-#                      File_formatted = "23-24_error5.csv",
-#                      base_dir = weather_path,
-#                      verbose = TRUE
+#    merge_weather(File_compressed = "/homevol/pmelloy/Weather observations/tgz/230825_1513_IDW60910.tgz",
+#                  station_file = "IDW60910.95641.json",
+#                  File_formatted = "23-24_error5.csv",
+#                  base_dir = weather_path,
+#                  verbose = TRUE
 #    )
